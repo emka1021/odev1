6506e0d3e55474b949f3f594e16462d308e65e81
emka1021 <131618895+emka1021@users.noreply.github.com>
2023-12-15 14:38:02 +0300

    odev
    odev

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.adiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyadiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bilgiler11BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personel11DataSet = new WindowsFormsApp4.personel11DataSet();
+            this.bilgiler11TableAdapter = new WindowsFormsApp4.personel11DataSetTableAdapters.bilgiler11TableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bilgiler11BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personel11DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.adiDataGridViewTextBoxColumn,
+            this.soyadiDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.bilgiler11BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(265, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(314, 88);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // adiDataGridViewTextBoxColumn
+            // 
+            this.adiDataGridViewTextBoxColumn.DataPropertyName = "adi";
+            this.adiDataGridViewTextBoxColumn.HeaderText = "adi";
+            this.adiDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.adiDataGridViewTextBoxColumn.Name = "adiDataGridViewTextBoxColumn";
+            this.adiDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // soyadiDataGridViewTextBoxColumn
+            // 
+            this.soyadiDataGridViewTextBoxColumn.DataPropertyName = "soyadi";
+            this.soyadiDataGridViewTextBoxColumn.HeaderText = "soyadi";
+            this.soyadiDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.soyadiDataGridViewTextBoxColumn.Name = "soyadiDataGridViewTextBoxColumn";
+            this.soyadiDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // bilgiler11BindingSource
+            // 
+            this.bilgiler11BindingSource.DataMember = "bilgiler11";
+            this.bilgiler11BindingSource.DataSource = this.personel11DataSet;
+            // 
+            // personel11DataSet
+            // 
+            this.personel11DataSet.DataSetName = "personel11DataSet";
+            this.personel11DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bilgiler11TableAdapter
+            // 
+            this.bilgiler11TableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bilgiler11BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personel11DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private personel11DataSet personel11DataSet;
+        private System.Windows.Forms.BindingSource bilgiler11BindingSource;
+        private personel11DataSetTableAdapters.bilgiler11TableAdapter bilgiler11TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyadiDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string constring = "Data Source=DESKTOP-N9LKLML\\SQLEXPRESS;Initial Catalog=personel11;Integrated Security=True";
+        SqlConnection Connect = new SqlConnection(constring);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personel11DataSet.bilgiler11' table. You can move, or remove it, as needed.
+            this.bilgiler11TableAdapter.Fill(this.personel11DataSet.bilgiler11);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Connect.State == ConnectionState.Closed)
+                {
+                    Connect.Open();
+                    string kayitekle = "insert into bilgiler11(adi, soyadi) values (@kisi_adi,@kisi_soyadi)";
+                    SqlCommand komut = new SqlCommand(kayitekle, Connect);
+                    komut.Parameters.AddWithValue("@kisi_adi", textBox1.Text);
+                    komut.Parameters.AddWithValue("@kisi_soyadi", textBox2.Text);
+
+                    komut.ExecuteNonQuery();
+                    Connect.Close();
+                    MessageBox.Show("işlem yapıldı");
+                }
+
+
+
+            }
+            catch (Exception uyari)
+            {
+                MessageBox.Show("uyarınız var" + uyari);
+
+                throw;
+            }
+        }
+    }
+}
